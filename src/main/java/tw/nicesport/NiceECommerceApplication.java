f06c7c98f2cdefb7b3de57320e009356a62980b5
constantem <marco65191013@gmail.com>
2022-04-09 01:28:15 +0800

    initial commit

---
+++ src/main/java/tw/nicesport/NiceECommerceApplication.java
@@ -0,0 +1,13 @@
+package tw.nicesport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceECommerceApplication.class, args);
+	}
+
+}

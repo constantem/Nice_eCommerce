dd0ff991f7ea34ee5182985dbb3edf673a5b536c
Steven <stevenhuang83816@gmail.com>
2022-04-26 10:13:49 +0800

    補推推

--- src/main/java/tw/nicesport/controller/OrderController.java
+++ src/main/java/tw/nicesport/controller/OrderController.java
@@ -13,15 +13,22 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import tw.nicesport.dto.CoachsAndRoomsContainer;
+import tw.nicesport.model.Coach;
+import tw.nicesport.model.Course;
 import tw.nicesport.model.OrderDetailBean;
 import tw.nicesport.model.OrdersBean;
+import tw.nicesport.model.Room;
 import tw.nicesport.service.OrderService;
 import tw.nicesport.service.OrderdDetailService;
+
 //管分頁的連線的物件
 @Controller
 public class OrderController {
@@ -81,33 +88,76 @@ public class OrderController {
 		return "/order/viewOrderDetail";
 	}
 	
-	//更新資料
-	@GetMapping("/orders/editOrders")
-	public String editOrders(Model model, @RequestParam(name="id") Integer id) {
-
-		OrdersBean orders = OrderService.findById(id);
-		model.addAttribute("Orders", orders);
-		
-		return "editOrders";
-	}
 	
-	@PostMapping("/orders/UpdateOrderShipInfo")
-	public ModelAndView editMessage(ModelAndView mav, @Valid @ModelAttribute(name="workMessage") OrdersBean orders, BindingResult br) {
-		
-		mav.setViewName("editOrders");
-		
-		if(!br.hasErrors()) {
-//			https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.entity-persistence.saving-entites
-			OrderService.insert(orders);
-			mav.setViewName("redirect:/message/viewMessages");
+	//更新配送資料
+	@RequestMapping("/orders/UpdateOrderShipInfo")
+	public String updateOneOrderShipInfo(
+//			@Valid   //驗證用  表示去驗證下面的OrdersBean
+			@ModelAttribute("order") OrdersBean ordersBean,
+			Model model) {	
+		Integer id = ordersBean.getOrder_id(); 
+		OrdersBean originalOrdersBean = OrderService.findById(id);
+		//把要更新的資料一個一個用set方法塞進去更新  ordersBean.getShipName()會拿到前端送過來的資料裡面的ShipName
+		//如果沒有set到的話資料都是null  會直接改到資料庫的資料
+		originalOrdersBean.setShipName(ordersBean.getShipName());
+		originalOrdersBean.setShipPostalCode(ordersBean.getShipPostalCode());
+		originalOrdersBean.setShipAddress(ordersBean.getShipAddress());
+		OrderService.updateOne(originalOrdersBean);
+		model.addAttribute("ordersBean", ordersBean);;
+//		redirect:是回到某個指定的Controller方法
+		return "redirect:/orders/OrderDetail?id=" + id;  
 		}
+	
+	
+	
+	
+	@RequestMapping("/orders/UpdateOrderState")
+	public String updateOneOederState(
+			@ModelAttribute("order") OrdersBean ordersBean,
+			Model model
+			){
+		Integer id = ordersBean.getOrder_id();
 		
-		return mav;
 		
+		return "redirect:/orders/OrderDetail?id=" + id;
 	}
 	
 	
-//	@getMapping("/orders/edit")
-//	public String editOrder(Model model,@ReequestOaram(name= ""))
-//		
+//	@RequestMapping("/orders/UpdateOrderState")
+//	public String updateOneCourse(
+////			@Valid   //驗證用  表示去驗證下面的OrdersBean
+//			@ModelAttribute("order") OrdersBean ordersBean,
+//			Model model) {	
+//		Integer id = ordersBean.getOrder_id(); 
+//		OrdersBean originalOrdersBean = OrderService.findById(id);
+//		//把要更新的資料一個一個用set方法塞進去更新  ordersBean.getShipName()會拿到前端送過來的資料裡面的ShipName
+//		//如果沒有set到的話資料都是null  會直接改到資料庫的資料
+//		originalOrdersBean.setShipName(ordersBean.getShipName());
+//		originalOrdersBean.setShipPostalCode(ordersBean.getShipPostalCode());
+//		originalOrdersBean.setShipAddress(ordersBean.getShipAddress());
+//		OrderService.updateOne(originalOrdersBean);
+//		model.addAttribute("ordersBean", ordersBean);;
+////		redirect:是回到某個指定的Controller方法
+//		return "redirect:/orders/OrderDetail?id=" + id;  
+//		}
+
+
+
+
 }
+
+//		if(br.hasErrors()) {
+//			model.addAttribute("OrdersBean", OrdersBean);
+//			model.addAttribute( "coachs", crContainer.getCoachs() );
+//			model.addAttribute( "rooms", crContainer.getRooms() );
+////			return "forward:/show/"+id;
+//			return "course/show-a-course";
+//		}
+
+//		if(status) {
+//			model.addAttribute("course", course);
+////			return "course/show-a-course";
+//			return "redirect:/order/viewOrderDetail?id="+id;
+//		}
+//
+//		return "course/update-failure";

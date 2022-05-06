07b66282b4adebdd136bd9a22d60ad3c1432cd00
john <eason416029@gmail>
2022-05-06 12:23:45 +0800

    圖表怎麼弄拉

--- src/main/java/tw/nicesport/model/CustomerBean.java
+++ src/main/java/tw/nicesport/model/CustomerBean.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @Entity
 @Table(name = "customersupport")
 public class CustomerBean {
